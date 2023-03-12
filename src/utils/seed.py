9b4c437d1397cfc93db560f674a59117f8968965
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-03-12 18:03:08 +0100

    Add utils module

---
+++ src/utils/seed.py
@@ -0,0 +1,26 @@
+"""
+Module containing a function to set the random seed for
+reproducibility
+"""
+import random
+import numpy as np
+import torch
+
+
+def set_random_seed(random_seed: int = 42) -> None:
+    """Set the random seed for reproducibility. The seed is set for the
+    random library, the numpy library and the pytorch library.
+
+    Parameters
+    ----------
+    random_seed : int, optional
+        The random seed to use for reproducibility, by default 42.
+    """
+    random.seed(random_seed)
+
+    np.random.seed(random_seed)
+
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
