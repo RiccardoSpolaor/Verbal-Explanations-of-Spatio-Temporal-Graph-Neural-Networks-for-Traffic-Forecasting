3020b33f7cc6d7d7b16d5c7553721079db4a8c84
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-03-29 12:54:44 +0200

    Rename trainer.py to training.py

--- src/spatial_temporal_gnn/training.py
+++ src/spatial_temporal_gnn/training.py
@@ -5,13 +5,15 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import numpy as np
+import matplotlib.pyplot as plt
 
-from .metrics import MAELoss, RMSELoss, S_MAPELoss
+from .metrics import MAE, RMSE, MAPE
 from .model import SpatialTemporalGNN
-from ..data.data_processer import Scaler
+from ..data.data_processing import Scaler
+
 
 class Checkpoint():
-    """Class to handle the checkpoints of a model."""
+    """Class to handle the checkpoints of a model"""
     def __init__(self, path: str, initial_error: float = float('inf')) -> None:
         """Initialize the checkpoint instance.
         
@@ -23,12 +25,12 @@ class Checkpoint():
             The initial error value, by default inf.
         """
         self.last_error = initial_error
-
+        
         self.path = path
         os.makedirs(os.path.dirname(self.path), exist_ok=True)
 
     def save_best(self, model: nn.Module, optimizer: torch.optim.Optimizer,
-                  new_error: float) -> None:
+                  new_error: float, **kwargs) -> None:
         """Possibly save the best model weights and optimizer state 
         in the checkpoint file according to the new value of the metric.
         
@@ -44,17 +46,21 @@ class Checkpoint():
         new_error : float
             The new error value which is compared to the best so far.
             The checkpoints are updated solely if the new error is less.
+        kwargs : Any
+            Named arguments saved in the checkpoints as ndarrays.
         """
         if new_error < self.last_error:
-            checkpoint = {}
+            checkpoint = dict()
             checkpoint['model_state_dict'] = model.state_dict()
             checkpoint['optimizer_state_dict'] = optimizer.state_dict()
             checkpoint['best_error'] = new_error
+            for k, v in kwargs.items():
+                checkpoint[k] = np.array(v)
 
             torch.save(checkpoint, self.path)
 
-        self.last_error = new_error
-
+            self.last_error = new_error
+    
     def load_best_weights(self, model: nn.Module) -> None:
         """Load the best weights on a model.
 
@@ -73,13 +79,13 @@ def train(
     lr_scheduler: Optional[object] = None,
     reload_best_weights: bool = True) -> Dict[str, np.ndarray]:
     # Initialize loss functions.
-    mae_function = MAELoss()
-    rmse_function = RMSELoss()
-    mape_function = S_MAPELoss()
+    mae_criterion = MAE()
+    rmse_criterion = RMSE()
+    mape_criterion = MAPE()
 
     # Initialize histories.
-    metrics = ['train_loss', 'train_mae', 'train_rmse', 'train_smape',
-           'val_loss', 'val_mae', 'val_rmse', 'val_smape']
+    metrics = ['train_mae', 'train_rmse', 'train_mape', 'val_mae', 'val_rmse',
+               'val_mape']
     history = { m: [] for m in metrics }
 
     # Set model in training mode.
@@ -93,43 +99,42 @@ def train(
         torch.cuda.empty_cache()
 
         # Initialize running loss and errors.
-        running_train_loss = 0.
         running_train_mae = 0.
         running_train_rmse = 0.
         running_train_mape = 0.
 
         start_time = time()
 
-        for batch_idx, data in enumerate(train_dataloader, 0):
+        for batch_idx, (x, y) in enumerate(train_dataloader):
             # Increment the number of batch steps.
             batch_steps = batch_idx + 1
 
             # Get the data.
-            x, y = data
             x = x.type(torch.float32).to(device=device)
             y = y.type(torch.float32).to(device=device)
 
             x = scaler.scale(x)
-
+            #print(x.shape)
             # Compute model predictions.
             y_pred = model(x)
+            #print(y_pred.shape, y.shape)
 
             # Compute the loss on the scaled results and ground truth.
-            loss = mae_function(y_pred, scaler.scale(y))
-            running_train_loss += loss.item()
-
             y_pred = scaler.un_scale(y_pred)
-            # Zero the gradients.
-            optimizer.zero_grad()
+
+            loss = mae_criterion(y_pred, y)
 
             # Compute errors and update running errors.
-            mae = mae_function(y_pred, y)
-            rmse = rmse_function(y_pred, y)
-            mape = mape_function(y_pred, y)
+            with torch.no_grad():
+                rmse = rmse_criterion(y_pred, y)
+                mape = mape_criterion(y_pred, y)
 
-            running_train_mae += mae.item()
+            running_train_mae += loss.item()
             running_train_rmse += rmse.item()
             running_train_mape += mape.item()
+
+            # Zero the gradients.
+            optimizer.zero_grad()
 
             # Use MAE as the loss function for backpropagation.
             loss.backward()
@@ -144,10 +149,9 @@ def train(
                 f'[{batch_steps}/{len(train_dataloader)}] -',
                 f'{epoch_time:.0f}s {batch_time * 1e3:.0f}ms/step -',
 
-                f'train {{ loss: {running_train_loss / batch_steps:.3g} -',
-                f'MAE: {running_train_mae / batch_steps:.3g} -',
+                f'train {{ MAE (loss): {running_train_mae / batch_steps:.3g} -',
                 f'RMSE: {running_train_rmse / batch_steps:.3g} -',
-                f'sMAPE: {running_train_mape * 100. / batch_steps:.3g}% }} -',
+                f'MAPE: {running_train_mape * 100. / batch_steps:.3g}% }} -',
 
                 f'lr: {optimizer.param_groups[0]["lr"]:.3g} -',
                 f'weight decay: {optimizer.param_groups[0]["weight_decay"]}',
@@ -157,47 +161,41 @@ def train(
         # Set the model in evaluation mode.
         model.eval()
 
-        train_loss = running_train_loss / len(train_dataloader)
         train_mae = running_train_mae / len(train_dataloader)
         train_rmse = running_train_rmse / len(train_dataloader)
-        train_smape = running_train_mape / len(train_dataloader)
+        train_mape = running_train_mape / len(train_dataloader)
 
-        history['train_loss'].append(train_loss)
         history['train_mae'].append(train_mae)
         history['train_rmse'].append(train_rmse)
-        history['train_smape'].append(train_smape)
+        history['train_mape'].append(train_mape)
 
-        val_results = validate(model, val_dataloader, mae_function, 
-                               rmse_function, mape_function, scaler, device)
-        val_loss, val_mae, val_rmse, val_smape = val_results
+        val_results = validate(model, val_dataloader, scaler, device)
+        val_mae, val_rmse, val_mape = val_results
 
-        history['val_loss'].append(val_loss)
         history['val_mae'].append(val_mae)
         history['val_rmse'].append(val_rmse)
-        history['val_smape'].append(val_smape)
+        history['val_mape'].append(val_mape)
 
         if checkpoint is not None:
-            err_sum = val_mae + val_rmse + val_smape
+            err_sum = val_mae + val_rmse + val_mape
             checkpoint.save_best(model, optimizer, err_sum)
 
         print(
             f'[{len(train_dataloader)}/{len(train_dataloader)}] -',
             f'{epoch_time:.0f}s -',
 
-            f'train: {{ loss: {train_loss:.3g} -',
-            f'MAE: {train_mae:.3g} -',
+            f'train: {{ MAE (loss): {train_mae:.3g} -',
             f'RMSE: {train_rmse:.3g} -',
-            f'sMAPE: {train_smape * 100.:.3g}% }} -',
+            f'MAPE: {train_mape * 100.:.3g}% }} -',
 
-            f'val: {{ loss: {val_loss:.3g} -',
-            f'MAE: {val_mae:.3g} -',
+            f'val: {{ MAE: {val_mae:.3g} -',
             f'RMSE: {val_rmse:.3g} -',
-            f'sMAPE: {val_smape * 100.:.3g}% }} -',
+            f'MAPE: {val_mape * 100.:.3g}% }} -',
 
             f'lr: {optimizer.param_groups[0]["lr"]:.3g} -',
             f'weight decay: {optimizer.param_groups[0]["weight_decay"]}')
 
-        lr_scheduler.step(train_loss)
+        lr_scheduler.step(train_mae)
 
         # Set model in training mode.
         model.train()
@@ -217,21 +215,21 @@ def train(
     return history
 
 def validate(
-    model: nn.Module, val_dataloader: DataLoader, mae_function: nn.Module,
-    rmse_function: nn.Module, mape_function: nn.Module, scaler: Scaler, device: str, 
+    model: nn.Module, val_dataloader: DataLoader, scaler: Scaler, device: str, 
     n_timestamps_to_predict: Optional[int] = None
     ) -> Tuple[float, float, float, float]:
     torch.cuda.empty_cache()
+    
+    mae_criterion = MAE()
+    rmse_criterion = RMSE()
+    mape_criterion = MAPE()
 
-    running_val_loss = 0.
     running_val_mae = 0.
     running_val_rmse = 0.
-    running_val_smape = 0.
+    running_val_mape = 0.
 
     with torch.no_grad():
-        for _, data in enumerate(val_dataloader, 0):
-            # Get the data.
-            x, y = data
+        for _, (x, y) in enumerate(val_dataloader):
             x = scaler.scale(x)
             x = x.type(torch.float32).to(device=device)
             y = y.type(torch.float32).to(device=device)
@@ -243,24 +241,51 @@ def validate(
                 y_pred = y_pred[:, : n_timestamps_to_predict]
                 y = y[:, : n_timestamps_to_predict]
 
-            loss = mae_function(y_pred, scaler.scale(y))
-            running_val_loss += loss.item()
-
             y_pred = scaler.un_scale(y_pred)
 
-            # Loss
-            mae = mae_function(y_pred, y)
-            rmse = rmse_function(y_pred, y)
-            mape = mape_function(y_pred, y)
+            mae = mae_criterion(y_pred, y)
+            rmse = rmse_criterion(y_pred, y)
+            mape = mape_criterion(y_pred, y)
+            
             running_val_mae += mae.item()
             running_val_rmse += rmse.item()
-            running_val_smape += mape.item()
+            running_val_mape += mape.item()
 
     torch.cuda.empty_cache()
 
-    val_loss = running_val_loss / len(val_dataloader)
     val_mae = running_val_mae / len(val_dataloader)
     val_rmse = running_val_rmse / len(val_dataloader)
-    val_smape = running_val_smape / len(val_dataloader)
+    val_mape = running_val_mape / len(val_dataloader)
 
-    return val_loss, val_mae, val_rmse, val_smape
+    return val_mae, val_rmse, val_mape
+
+def _plot_subplot(index: int, train_history, val_history, title, metric_name):
+    plt.subplot(*index)
+    
+    plt.title(title)
+
+    plt.plot(train_history, label='train')
+    plt.plot(val_history, label='validation')
+
+    plt.xlabel('epochs')
+
+    plt.ylabel(metric_name)
+    
+    plt.legend()
+    
+
+def plot_training_history(history):
+    plt.figure(figsize=(15, 10))
+
+    _plot_subplot((2, 1, 1), history['train_mae'], history['val_mae'],
+                  'Train and validation MAE history', 'MAE')
+    _plot_subplot((2, 2, 3), history['train_rmse'], history['val_rmse'],
+                  'Train and validation RMSE history', 'RMSE')
+    _plot_subplot((2, 2, 4), history['train_mape'] * 100.,
+                  history['val_mape'] * 100.,
+                  'Train and validation MAPE history', 'MAPE (%)')
+    
+    plt.suptitle('Training and validation history', size=16)
+    
+    plt.tight_layout()
+    plt.show()
