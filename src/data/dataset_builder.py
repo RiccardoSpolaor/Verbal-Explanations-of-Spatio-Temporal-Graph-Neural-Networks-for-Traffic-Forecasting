656f801ce21572d801fea87d2c572e6370556206
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-03-30 12:11:02 +0200

    Update dataset_builder.py
    - Add docstring.
    - FIx split.

--- src/data/dataset_builder.py
+++ src/data/dataset_builder.py
@@ -2,19 +2,31 @@ from typing import Tuple
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-def get_node_values_and_adjacency_matrix(
-    node_values_file_path: str, adjacency_matrix_file_path: str
-    ) -> Tuple[np.ndarray, np.ndarray]:
-    return np.load(node_values_file_path), np.load(adjacency_matrix_file_path)
-
 def train_test_val_split(
     node_values: np.ndarray, test_size: float = .2,
     val_size: float = .1) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    initial_dataset_len = node_values.shape[0]
+    """Apply the train, validation and test split of a dataset.
+
+    Parameters
+    ----------
+    node_values : ndarray
+        The node values dataset to split in train test and validation
+    test_size : float, optional
+        The test ratio, by default 0.2.
+    val_size : float, optional
+        The validation ratio, by default 0.1.
+
+    Returns
+    -------
+    ndarray
+        The train dataset.
+    ndarray
+        The validation dataset.
+    ndarray
+        The test dataset.
+    """
     x_train, x_test = train_test_split(node_values, test_size=test_size,
                                        shuffle=False)
-    split_dataset_len = x_train.shape[0]
-    new_val_size = val_size * split_dataset_len / initial_dataset_len
-    x_train, x_val = train_test_split(x_train, test_size=new_val_size,
+    x_train, x_val = train_test_split(x_train, test_size=val_size,
                                       shuffle=False)
     return x_train, x_val, x_test
